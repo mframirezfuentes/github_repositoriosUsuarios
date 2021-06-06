c2318fd9a39091617205cc25bd1446c2e5ff6035
María Fernanda Ramírez <mframirezfuentes@gmail.com>
2021-06-06 16:12:56 -0400

    se ven todos los respositorios dependiendo de cuantos uno coloque

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,39 +4,38 @@ import {
 
 let form = document.querySelector('form');
 form.disabled = true;
+let arreglo = [];
 const mostrarDOM = () => {
-    const cuerpo = document.getElementById('resultados');
+    let cuerpo = document.getElementById('resultados');
     const boton = document.getElementById('btnBoton');
     boton.addEventListener('click', async () => {
         try {
             const user = document.getElementById('nombre');
             const pagina = document.getElementById('pagina');
             const cantidad_repos = document.getElementById('repoPagina');
-            if (user.value != "" && pagina.value !="" && cantidad_repos.value !="") {
+            if (user.value != "" && pagina.value != "" && cantidad_repos.value != "") {
                 const datos = await request(user.value, pagina.value, cantidad_repos.value)
-                cuerpo.innerHTML = `<div class="row">                
+                let info = datos[1];
+                arreglo.push(info)
+                cuerpo.innerHTML = `<div class="row">               
                 <div class="col-6">
                 <h2>Datos Usuarios</h2>
                 <p>Nombre Usuario: ${datos[0].name}</p>
                 <p>Nombre de login: ${datos[0].login}</p>
                 <p> Cantidad de repositorios: ${datos[0].public_repos}</p>
                 <p>Localidad: ${datos[0].location}</p>
-                <p> Tipo de Usuario: ${datos[0].type}</p>
-                </div>
-                <div class="col-6">
-                <h2>Datos Repositorios</h2>
-               <a href=${datos[1][0].html_url}>${datos[1][0].name}</a><br>
-               <a href=${datos[1][1].html_url}>${datos[1][1].name}</a><br>
-               <a href=${datos[1][2].html_url}>${datos[1][2].name}</a><br>
-               <a href=${datos[1][3].html_url}>${datos[1][3].name}</a><br>
-               <a href=${datos[1][4].html_url}>${datos[1][4].name}</a><br>
-               <a href=${datos[1][5].html_url}>${datos[1][5].name}</a><br>
-               <a href=${datos[1][6].html_url}>${datos[1][6].name}</a><br>
-               <a href=${datos[1][7].html_url}>${datos[1][7].name}</a><br>
-               <a href=${datos[1][8].html_url}>${datos[1][8].name}</a><br>
-               <a href=${datos[1][9].html_url}>${datos[1][9].name}</a><br>          
-               </div>
+                <p> Tipo de Usuario: ${datos[0].type}</p> 
+                </div> 
+                <div class="col-6" id="repositorio">
+                <h2 id="datos">Datos Repositorios</h2><br>
+                </div>  
                 </div>`
+                console.log(arreglo)
+                for (let i = 0; i < arreglo[0].length; i++) {
+                    console.log(arreglo[0][i].html_url)
+                    document.getElementById('datos').innerHTML += `<br>     
+                          <a href=${arreglo[0][i].html_url}>${arreglo[0][i].name}</a>`
+                }
             }
         } catch (error) {
             alert('Not Found')
